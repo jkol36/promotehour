4f179385f5d46e7677ec93a16986b5163c763acd
Jon Kolman <jonathankolman@gmail.com>
2015-09-24 23:53:33 +0300

    finished

--- start.py
+++ start.py
@@ -1,27 +1,19 @@
-from twisted.internet import reactor
+import os
+import sys
 from promotehour.spiders import betabound_scraper, betalist_scraper, producthunt_spider, startuplist_scraper, startuplister_scraper
-from scrapy.crawler import CrawlerRunner
-from scrapy.utils.project import get_project_settings
-import Queue
-
-q = Queue.Queue()
-runner = CrawlerRunner()
-
-crawlers = [betabound_scraper.BetaBoundScraper, betalist_scraper.BetaListScraper, producthunt_spider.ProductHuntSpider, startuplister_scraper.StartupListerScraper, startuplist_scraper.StartupListScraper]
-
-for crawler in crawlers:
-	runner.crawl(crawler)
-
-d = runner.join()
-d.addBoth(lambda _:reactor.stop())
-reactor.run()
+from Queue import Queue
 #	q.put(process.crawl)
 
+q = Queue()
+sites = ["producthunt", "betalist", "startuplist", "startuplister", 'betabound']
+for site in sites:
+	q.put(os.system("scrapy crawl {}".format(site)))
 
-#q.join()
-#while True:
-#	process = q.get(timeout=1)
-#	if process:
-#		process.start()
-#	else:
-#		break
+q.join()
+while True:
+	try:
+		crawler = q.get(timeout=1)
+	except Exception, e:
+		break
+
+	crawler.run()
